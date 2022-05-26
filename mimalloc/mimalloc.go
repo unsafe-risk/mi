837f11b864184860995d79304dca5595ff259090
snowmerak <snowmerak@outlook.com>
2022-05-26 23:19:47 +0900

    wrap elementary alloc function

--- mimalloc/mimalloc.go
+++ mimalloc/mimalloc.go
@@ -5,3 +5,24 @@ package mimalloc
 #include <./src/static.c>
 */
 import "C"
+import "unsafe"
+
+func Malloc(size int) unsafe.Pointer {
+	return C.mi_malloc(C.size_t(size))
+}
+
+func Zalloc(size int) unsafe.Pointer {
+	return C.mi_zalloc(C.size_t(size))
+}
+
+func Calloc(count, size int) unsafe.Pointer {
+	return C.mi_calloc(C.size_t(count), C.size_t(size))
+}
+
+func Realloc(ptr unsafe.Pointer, size int) unsafe.Pointer {
+	return C.mi_realloc(ptr, C.size_t(size))
+}
+
+func Free(ptr unsafe.Pointer) {
+	C.mi_free(ptr)
+}
