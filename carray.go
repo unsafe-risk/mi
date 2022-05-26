45b183b173d3d6ad826b66884b38ca5c7985c85e
snowmerak <snowmerak@outlook.com>
2022-05-27 00:14:17 +0900

    Create carray.go

---
+++ carray.go
@@ -0,0 +1,32 @@
+package mi
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type CArray[T any] struct {
+	ptr  unsafe.Pointer
+	len  int
+	size int
+}
+
+func (c *CArray[T]) Length() int {
+	return c.len
+}
+
+func (c *CArray[T]) At(idx int) (T, error) {
+	if idx >= c.len {
+		var zero T
+		return zero, fmt.Errorf("CArray.At: %w: %d", outOfRange, idx)
+	}
+	return *(*T)(unsafe.Pointer(uintptr(c.ptr) + uintptr(idx)*uintptr(c.size))), nil
+}
+
+func (c *CArray[T]) Set(idx int, value T) error {
+	if idx >= c.len {
+		return fmt.Errorf("CArray.Set: %w: %d", outOfRange, idx)
+	}
+	*(*T)(unsafe.Pointer(uintptr(c.ptr) + uintptr(idx)*uintptr(c.size))) = value
+	return nil
+}
