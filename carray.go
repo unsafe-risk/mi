a5d5ca22ceeadf547aad25fbdf300e010097b13c
snowmerak <snowmerak@outlook.com>
2022-05-27 12:28:06 +0900

    toSlice method to CArray

--- carray.go
+++ carray.go
@@ -2,6 +2,7 @@ package mi
 
 import (
 	"fmt"
+	"reflect"
 	"unsafe"
 )
 
@@ -29,4 +30,12 @@ func (c *CArray[T]) Set(idx int, value T) error {
 	}
 	*(*T)(unsafe.Pointer(uintptr(c.ptr) + uintptr(idx)*uintptr(c.size))) = value
 	return nil
+}
+
+func (c *CArray[T]) ToSlice() []T {
+	result := []T{}
+	(*reflect.SliceHeader)(unsafe.Pointer(&result)).Data = uintptr(c.ptr)
+	(*reflect.SliceHeader)(unsafe.Pointer(&result)).Len = c.len
+	(*reflect.SliceHeader)(unsafe.Pointer(&result)).Cap = c.len
+	return result
 }
