dc0cc121aa5f7ad82f73ea17bb0865899b3dc8a4
snowmerak <snowmerak@outlook.com>
2022-05-27 11:45:54 +0900

    add alloced var

--- alloc.go
+++ alloc.go
@@ -1,32 +1,44 @@
 package mi
 
 import (
+	"sync/atomic"
 	"unsafe"
 
 	"github.com/unsafe-risk/mi/mimalloc"
 )
 
+func Alloced() int64 {
+	return atomic.LoadInt64(&alloced)
+}
+
 func MAlloc(size int) unsafe.Pointer {
+	atomic.AddInt64(&alloced, int64(size))
 	return mimalloc.Malloc(size)
 }
 
 func MAllocOf[T any]() *T {
 	var a T
-	return (*T)(mimalloc.Malloc(int(unsafe.Sizeof(a))))
+	size := unsafe.Sizeof(a)
+	atomic.AddInt64(&alloced, int64(size))
+	return (*T)(mimalloc.Malloc(int(size)))
 }
 
 func MAllocSmart[T any]() *SmartPointer[T] {
 	var a T
-	return NewSmartPointer[T](mimalloc.Malloc(int(unsafe.Sizeof(a))))
+	size := unsafe.Sizeof(a)
+	atomic.AddInt64(&alloced, int64(size))
+	return NewSmartPointer[T](mimalloc.Malloc(int(size)))
 }
 
 func CAlloc(count, size int) unsafe.Pointer {
+	atomic.AddInt64(&alloced, int64(count*size))
 	return mimalloc.Calloc(count, size)
 }
 
 func CAllocOf[T any](count int) CArray[T] {
 	var a T
 	size := int(unsafe.Sizeof(a))
+	atomic.AddInt64(&alloced, int64(count*size))
 	rs := CArray[T]{
 		ptr:  mimalloc.Calloc(count, size),
 		len:  count,
