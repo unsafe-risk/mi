c77b13b3baf9a195c56c550d30797c60a4449454
snowmerak <snowmerak@outlook.com>
2022-05-27 11:49:15 +0900

    add alloced variable to free function

--- free.go
+++ free.go
@@ -1,6 +1,7 @@
 package mi
 
 import (
+	"sync/atomic"
 	"unsafe"
 
 	"github.com/unsafe-risk/mi/mimalloc"
@@ -12,8 +13,10 @@ func Free(ptr unsafe.Pointer) {
 
 func FreeOf[T any](ptr *T) {
 	mimalloc.Free(unsafe.Pointer(ptr))
+	atomic.AddInt64(&alloced, -int64(unsafe.Sizeof(*ptr)))
 }
 
 func FreeCArray[T any](ptr CArray[T]) {
 	mimalloc.Free(ptr.ptr)
+	atomic.AddInt64(&alloced, -int64(ptr.len*ptr.size))
 }
