032ab64c9c4ead80c0cb5f93ad8123e6d2f414d3
snowmerak <snowmerak@outlook.com>
2022-06-12 08:58:21 +0900

    Revert "update arena for mimalloc"
    This reverts commit bb9906ab21a710ec0097b8ba0a0e7a552949e1e2.

--- arena/arena.go
+++ arena/arena.go
@@ -3,6 +3,7 @@ package arena
 import (
 	"reflect"
 	"runtime"
+	"syscall"
 	"unsafe"
 
 	"github.com/unsafe-risk/mi/mimalloc"
@@ -10,10 +11,15 @@ import (
 
 // This code is implemented by lemon-mint.
 // I brought the code from unsafe-risk/umem.
-// I edit a lot of the code to make it work with mimalloc's arena.
 
+// This Implementation is based on the proposal in the following url: https://github.com/golang/go/issues/51317
+
+// Thread-unsafe Allocation Arena.
 type Arena struct {
-	allocatedPointers []unsafe.Pointer
+	// The start address of the region.
+	head uintptr
+	// Tail of the region.
+	tail uintptr
 }
 
 func NewFinalizer() *Arena {
@@ -31,16 +37,106 @@ func arenaFinalizer(a *Arena) {
 	a.Free()
 }
 
-func (r *Arena) Free() {
-	for _, p := range r.allocatedPointers {
-		mimalloc.Free(p)
+// Page Structure
+/*
+	|  0  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |
+	|-----|-----|-----|-----|-----|-----|-----|-----|
+	|  page size            |  page head            |
+	|-----|-----|-----|-----|-----|-----|-----|-----|
+	|  Next Page Ptr                                |
+	|-----|-----|-----|-----|-----|-----|-----|-----|
+	|                                               |
+	|                                               |
+	|                                               |
+	|                      Data                     |
+	|                                               |
+	|                                               |
+	|                                               |
+	|-----|-----|-----|-----|-----|-----|-----|-----|
+*/
+
+var defaultPageSize = syscall.Getpagesize()*4 - 16
+
+func (r *Arena) newPage(size int) {
+	// println("Allocating new page", size)
+	sptr := mimalloc.Malloc(size + 16)
+	pagesize := (*uint32)(unsafe.Pointer(sptr))
+	pagehead := (*uint32)(unsafe.Pointer(uintptr(sptr) + 4))
+	nextpage := (*uint64)(unsafe.Pointer(uintptr(sptr) + 8))
+
+	*pagesize = uint32(size)
+	*pagehead = 0
+	*nextpage = 0
+
+	if r.tail != 0 {
+		// Add to the tail of the region.
+		tailNextPage := (*uint64)(unsafe.Pointer(r.tail + 8))
+		if *tailNextPage != 0 {
+			*nextpage = *tailNextPage
+		}
+		*tailNextPage = uint64(uintptr(sptr))
 	}
+	r.tail = uintptr(sptr)
+	if r.head == 0 {
+		r.head = uintptr(sptr)
+	}
+	// println("New page allocated", size, sptr)
+}
+
+func (r *Arena) Reset() {
+	r.tail = r.head
+	for r.head != 0 {
+		pagehead := (*uint32)(unsafe.Pointer(r.head + 4))
+		nextpage := (*uint64)(unsafe.Pointer(r.head + 8))
+		*pagehead = 0
+		r.head = uintptr(*nextpage)
+	}
+	r.head = r.tail
+}
+
+func (r *Arena) Free() {
+	for r.head != 0 {
+		_ = (*uint32)(unsafe.Pointer(r.head))
+		nextpage := (*uint64)(unsafe.Pointer(r.head + 8))
+		nexthead := uintptr(*nextpage)
+		mimalloc.Free(unsafe.Pointer(r.head))
+		r.head = nexthead
+	}
+	r.tail = 0
 }
 
 func (r *Arena) allocate(size int) uintptr {
-	p := mimalloc.Malloc(size)
-	r.allocatedPointers = append(r.allocatedPointers, p)
-	return uintptr(p)
+retry:
+	if r.tail == 0 {
+		// println("tail is 0, allocating new page")
+		if size > defaultPageSize {
+			r.newPage(size)
+		} else {
+			r.newPage(defaultPageSize)
+		}
+	}
+
+	pagesize := (*uint32)(unsafe.Pointer(r.tail))
+	pagehead := (*uint32)(unsafe.Pointer(r.tail + 4))
+	nextpage := (*uint64)(unsafe.Pointer(r.tail + 8))
+	if *pagesize-*pagehead < uint32(size) {
+		if *nextpage != 0 {
+			r.tail = uintptr(*nextpage)
+			goto retry
+		}
+		if size > defaultPageSize {
+			r.newPage(size)
+		} else {
+			r.newPage(defaultPageSize)
+		}
+		pagesize = (*uint32)(unsafe.Pointer(r.tail))
+		pagehead = (*uint32)(unsafe.Pointer(r.tail + 4))
+		nextpage = (*uint64)(unsafe.Pointer(r.tail + 8))
+	}
+
+	data := r.tail + 16 + uintptr(*pagehead)
+	*pagehead += uint32(size)
+	return data
 }
 
 func (r *Arena) NewBytes(size int) []byte {
